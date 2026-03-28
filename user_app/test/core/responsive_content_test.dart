0d31943a46e8eb783c07176ef5b82ac0a73b79dd
susin-d <181512045+susin-d@users.noreply.github.com>
2026-03-28 18:06:57 +0530

    tests

--- user_app/test/core/responsive_content_test.dart
+++ user_app/test/core/responsive_content_test.dart
@@ -1,6 +1,3 @@
-void main() {
-  // Placeholder main to prevent test runner errors.
-}
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile_app/core/widgets/responsive_content.dart';
